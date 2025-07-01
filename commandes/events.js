0faaab70d166c5fedb30b7a87544bb7c39fcbade
Mickeydeveloper <mickdenzel24@gmail.com>
2025-07-01 21:40:20 +0300

    Update events.js

--- commandes/events.js
+++ commandes/events.js
@@ -4,19 +4,19 @@ const { attribuerUnevaleur } = require('../bdd/welcome');
 
 async function events(nomCom) {
     zokou({
-        nomCom: nomCom,
+        nomCom: "events",
         categorie: 'Group'
     }, async (dest, zk, commandeOptions) => {
         const { ms, arg, repondre, superUser, verifAdmin } = commandeOptions;
 
         if (verifAdmin || superUser) {
             if (!arg[0] || arg.join(' ') === ' ') {
-                repondre(nomCom + ' ' + ' on to active and ' + ' ' + nomCom + ' ' + 'off to put off');
+                repondre(events + ' ' + ' on to active and ' + ' ' + events + ' ' + 'off to put off');
             } else {
                 if (arg[0] === 'on' || arg[0] === 'off') {
                     
                     await attribuerUnevaleur(dest, nomCom, arg[0]);
-                    repondre( nomCom + "is actualised on " + arg[0]);
+                    repondre( events + "is actualised on " + arg[0]);
                 } else {
                     repondre('on for active and off for desactive');
                 }
